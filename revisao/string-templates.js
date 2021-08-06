14be48b861c3914e26364093f9176a2fd087b316
kefflen <55823300+kefflen@users.noreply.github.com>
2021-08-06 09:37:19 -0300

    Create string-templates.js

---
+++ revisao/string-templates.js
@@ -0,0 +1,13 @@
+let x = 2
+console.log(`${x.toFixed(2)}`)
+let usuario = "aaaa"
+let senha = "aaaa"
+let nomeCompleto = "aaaa"
+let idade = "aaaa"
+
+let infoUser = {
+    usuario,
+    senha,
+    nomeCompleto,
+    idade
+}
