f32b7cf3afa0b7faf90c817a7ca150d5a5237e2d
kefflen <55823300+kefflen@users.noreply.github.com>
2021-08-13 10:01:55 -0300

    criando components

---
+++ revisao/revisao.js
@@ -0,0 +1,15 @@
+
+let a = [
+    1,2,3,4, [5,6,7,8,9, [10, 11, 12], [13, 14, 15], [16,17,18]]
+]
+function rasterizar(arr) {
+    let result = []
+    for (item of arr) {
+        if (Array.isArray(item)) {
+
+        } else {
+
+        }
+    }
+    return
+}
